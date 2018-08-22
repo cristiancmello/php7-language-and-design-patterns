6cf610cbe24ac3d4df0badbcee2c2cb4711458e3
Cristian Mello <cristianc.mello@gmail.com>
2018-08-22 19:28:43 -0300

    Add string operators

---
+++ the-language/operators/10-string-operators.php
@@ -0,0 +1,14 @@
+<?php
+
+// String Operators
+// Há 2 operadores de string: `.` e `.=`
+
+// Concatenação
+$a = 'Hello ';
+echo ($a . 'World!') . "\n"; // Hello World!
+
+$a .= 'World!';
+echo $a . "\n"; // Hello World!
+
+// String Functions
+// https://secure.php.net/manual/en/ref.strings.php
