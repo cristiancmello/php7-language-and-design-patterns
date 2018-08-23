f1a88cdacc63a7efffeac49f510dbaaef5ba7374
Cristian Mello <cristianc.mello@gmail.com>
2018-08-23 19:50:05 -0300

    Add while loop

---
+++ the-language/control-structures/2-while.php
@@ -0,0 +1,37 @@
+<?php
+
+// While
+// Laços `while` tem mesma sintaxe e semântica que em C
+
+// Exemplo 1
+$i = 0;
+
+while ($i < 10) {
+    echo $i++ . "\n";
+}
+
+// Exemplo 2
+$i = 0;
+
+while ($i < 10):
+    echo $i . "\n";
+    $i++;
+endwhile;
+
+// Do...while
+// Mesma sintaxe que em C.
+
+$i = 0;
+
+do {
+    echo $i++ . "\n";
+} while ($i < 10);
+
+// Parar a execução no meio de um bloco com `break`.
+$i = 0;
+do {
+    echo $i++ . "\n";
+
+    if ($i > 10) break;
+
+} while (true);
