11d087b79b786246096b4a403992a35c7d48cf40
Cristian Mello <cristianc.mello@gmail.com>
2018-08-23 21:31:12 -0300

    Add switch (basic)

---
+++ the-language/control-structures/7-switch.php
@@ -0,0 +1,17 @@
+<?php
+
+// Switch
+// Pode substituir a uma série de IFs na mesma expressão.
+
+$i = 0;
+
+switch ($i) {
+    case 0:
+        echo "i equals 0" . "\n";
+        break;
+    case 1:
+        echo "i equals 1" . "\n";
+        break;
+    default:
+        echo "i equals $i" . "\n";
+}
