302811317cfc19d2392c198a02565ae242ca6150
Cristian Mello <cristianc.mello@gmail.com>
2018-10-08 20:39:30 -0300

    add functions/5-internal.php

---
+++ the-language/functions/5-internal.php
@@ -0,0 +1,8 @@
+<?php
+
+// Internal Functions
+// PHP vem com coleção de funções e construtores.
+// Existem funções que necessitam do carregamento de extensões específicas compiladas senão
+// mensagens como `Undefined function` irão aparecer.
+
+// Exemplo: a função `imagecreatetruecolor()` precisa que a extensão GD esteja carregada
