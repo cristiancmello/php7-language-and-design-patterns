6daa229705eef9003ca851bc52ecf3d01d376861
Cristian Mello <cristianc.mello@gmail.com>
2018-10-14 10:20:13 -0300

    Add oop/15-overloading.php

---
+++ the-language/oop/15-overloading.php
@@ -0,0 +1,9 @@
+<?php
+
+// OVERLOADING
+// PHP provê recursos para criar dinamicamente propriedades e métodos.
+// (*) As entidades criadas dinamicamente são processadas por métodos mágicos
+// (*) Fornece a uma classe vários tipos de ações
+// (*) PHP interpreta o termo "sobrecarga" de modo diferente da maioria das linguagens que suportam OOP
+// (*) Muitos autores preferem chamar "OVERLOADING" do PHP como "Interpreter hooks"
+// Referência: https://secure.php.net/manual/en/language.oop5.overloading.php
